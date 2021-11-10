44a950d33f96e2fe79a9a8432d731687e7679499
Surajsingh419 <83262874+Surajsingh419@users.noreply.github.com>
2021-11-10 21:21:03 +0530

    Add files via upload
    my second file loggger

---
+++ logger.js
@@ -0,0 +1,13 @@
+function log (name) {
+console.log('the name is' +name)
+
+}
+function welcome(){
+console.log('welcome to my application. i am suraj and i am a part of functionup.')
+}
+
+let url = 'https://www.google.com'
+
+module.exports.logMessage = log
+module.exports.printWelcomeMessage = welcome
+module.exports.loggerendpoint = url
