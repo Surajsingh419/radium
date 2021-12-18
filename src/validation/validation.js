5c37160aab4e8d144ccc7ea2628713eac5bc38a9
surajsingh419 <sursingh1996@gmail.com>
2021-12-18 08:43:52 +0530

    First commit

---
+++ src/validation/validation.js
@@ -0,0 +1,13 @@
+const isValidReqBody = function(reqBody)
+{ 
+    return Object.keys(reqBody).length > 0 ;     
+}
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+
+
+
+module.exports={isValidReqBody,isValid}
